126dea451ac1545fa6a804294caf9a375b5850f7
Anders Latif <andl@kea.dk>
2022-02-01 10:44:48 +0100

    Added the intro exercises for homework

---
+++ 1/0._Intro/1._Intro_Exercises/1.Strings.js
@@ -0,0 +1,54 @@
+// --------------------------------------
+// Exercise 1 - Add numbers from string to float
+
+const numberOne = "1.10";
+const numberTwo = "2.30";
+
+// add those two numbers and show the result
+// you cannot touch line 1 neither line 2
+
+
+// --------------------------------------
+
+
+// --------------------------------------
+// Exercise 2 - Add the numbers and the total with 2 decimals
+
+const anotherNumberOne = "1.10";
+const anotherNumberTwo = "2.30";
+
+
+// --------------------------------------
+// Exercise 3 - Decimals and average
+
+const one = 10;
+const two = 45;
+const three = 98;
+
+// Show in the console the avg. with 5 decimals
+
+
+
+
+
+// --------------------------------------
+// Exercise 4 - Get the character by index
+
+const letters = "abc";
+// Get me the character "c"
+
+
+
+
+// --------------------------------------
+// Exercise 5 - Replace
+
+const fact = "You are learning javascript!";
+
+// capitalize the J in Javascript
+
+
+// --------------------------------------
+
+
+
