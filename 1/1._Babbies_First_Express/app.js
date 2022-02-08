288afcf1cfe9d8f0b1d017395cb3830920968541
Anders Latif <andl@kea.dk>
2022-02-08 11:46:30 +0100

    Finished the babbies first server by now

--- 1/1._Babbies_First_Express/app.js
+++ 1/1._Babbies_First_Express/app.js
@@ -12,12 +12,19 @@ app.get("/", (req, res) => {
     res.send({ message: "Welcome to my server. This is my first route ever." });
 });
 
+app.get("/about", (req, res) => {
+    res.send({  
+        version: "0.0.1"
+    });
+});
+
 app.post("/", (req, res) => {
     console.log(req.body);
     res.send(req.body);
 });
 
-// this is not REST
+
+// this is not REST!!!
 // task create a GET route called about that sends some info about the server
 // task implement a POST request handler on the endpoint /opinion based on the client data the server sends a response that contains an opinion 
 
