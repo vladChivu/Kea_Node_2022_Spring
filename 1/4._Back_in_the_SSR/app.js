484ea26bfea7f6419da90d69c1d7444eb1a19c85
Anders Latif <andl@kea.dk>
2022-02-22 11:46:52 +0100

    Started the SSR project for class 1

--- 1/4._Back_in_the_SSR/app.js
+++ 1/4._Back_in_the_SSR/app.js
@@ -3,12 +3,23 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/frontpage/frontpage.html");
-});
+const fs = require("fs");
+
+const nav = fs.readFileSync("./public/components/nav.html").toString();
+const footer = fs.readFileSync("./public/components/footer.html").toString();
+
+const frontpage = fs.readFileSync("./public/pages/frontpage/frontpage.html").toString();
+const cheesegallery = fs.readFileSync("./public/pages/cheesegallery/cheesegallery.html").toString(); 
+
+const frontpagePage = nav.replace("%%DOCUMENT_TITLE%%", "Welcome to the cheese zone") + frontpage + footer;
+const cheesegalleryPage = nav.replace("%%DOCUMENT_TITLE%%", "You are in the cheese zone") + cheesegallery + footer;
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/cheese/cheese.html");
+    res.send(frontpagePage);
+});
+
+app.get("/cheesegallery", (req, res) => {
+    res.send(cheesegalleryPage)
 });
 
 app.listen(3000, () => console.log("Server is running on", 3000));
