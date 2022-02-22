a01a368d1ce6c3726ab2806ff3fc505f2437c1ad
Anders Latif <andl@kea.dk>
2022-02-22 14:56:45 +0100

    Finished the WWE project for now

--- 2/3._World_Wide_Express/app.js
+++ 2/3._World_Wide_Express/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));   
+
+
 app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/frontpage/frontpage.html");
+});
+
+app.get("/welcome", (req, res) => {
     res.send(`
         <h1>Welcome to my website</h1>
         <h2>Take a look ${true}</h2>
