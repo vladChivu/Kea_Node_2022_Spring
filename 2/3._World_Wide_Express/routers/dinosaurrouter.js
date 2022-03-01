d560fa5800e1e45432d718dd79f4573bec389363
Anders Latif <andl@kea.dk>
2022-03-01 15:47:01 +0100

    Finished work on the dinosaurs router for now

--- 2/3._World_Wide_Express/routers/dinosaurrouter.js
+++ 2/3._World_Wide_Express/routers/dinosaurrouter.js
@@ -1,10 +1,21 @@
 const router = require("express").Router();
+const { calculateAmountOfCoolDinosaurs } = require("../dinosaurs/dinosaurs.js");
 
-
+// task solve line 5 so it gives the correct number
 router.get("/calculatecooldinosaurs", (req, res) => {
-    res.send({ data: "Test? Does it work?" });
+    res.send({ data: calculateAmountOfCoolDinosaurs() });
 });
 
+router.get("/amountofcooldinosaurs", (req, res) => {
+    res.redirect("/calculatecooldinosaurs");
+});
+
+router.get("/coolestdinosaur", (req, res) => {
+    if (req.query.cool === "yes") {
+        return res.send({ dinosaur: "Mosasaurus" });
+    }
+    res.send({ dinosaur: "T. Rex" });
+});
 
 
 module.exports = {
