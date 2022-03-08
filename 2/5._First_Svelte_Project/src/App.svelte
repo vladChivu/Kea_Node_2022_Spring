ed338c9240c5351d7e3d2d3a682246d54f7257ea
Anders Latif <andl@kea.dk>
2022-03-08 14:45:00 +0100

    Created the Tree page

--- 2/5._First_Svelte_Project/src/App.svelte
+++ 2/5._First_Svelte_Project/src/App.svelte
@@ -1,10 +1,15 @@
 <script>
 	export let name;
+
+	import Tree from "./pages/Tree/Tree.svelte";
+
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<Tree />
+	
 </main>
 
 <style>
