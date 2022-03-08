ea8e497d551bd886c386b00336e10854eaf4bb31
Anders Latif <andl@kea.dk>
2022-03-08 13:12:57 +0100

    Solved the browser fetch and node-fetch assignments

--- 2/3._World_Wide_Express/public/frontpage/frontpage.js
+++ 2/3._World_Wide_Express/public/frontpage/frontpage.js
@@ -1,1 +1,9 @@
 console.log("rrRRRrrrrRRRAaaaaaaAAwwwwWWWWw");
+
+fetch("/calculatecooldinosaurs")
+.then(response => response.json())
+.then(result => {
+    const favoriteDinosaurAmount = document.getElementById("cool-dinosaurs");
+
+    favoriteDinosaurAmount.innerText = result.data;
+});
