631e1da638e6d5d8e37ffcea50646c77287a3bb8
Anders Latif <andl@kea.dk>
2022-03-08 09:08:39 +0100

    Worked with fetch and node-fetch

--- 1/3._World_Wide_Express/app.js
+++ 1/3._World_Wide_Express/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const fetch = require("node-fetch");
+
 /* const animalsUtils = require("./animals/animalsUtils.js");
 console.log("You like this many animals:", animalsUtils.calcuateFavoriteAnimals()); */
 
@@ -21,6 +23,23 @@ app.get("/weather", (req, res) => {
     res.sendFile(__dirname + "/public/weather.html");
 });
 
+
+app.get("/proxy", async (req, res) => {
+    const response = await fetch("https://www.google.com")
+    const website = await response.text();
+    res.send(website);
+});
+
+/* Assignment
+    use node-fetch before version 3.0.0 to use require to import
+    create a route with the endpoint /proxy
+    fetch google's homepage
+    Use response.text() to parse the response
+    and display it to the client who requests /proxy
+
+    Bonus: use async/await to make it look nicer
+
+*/
 
 const PORT = process.env.PORT || 6000;
 
--- 1/3._World_Wide_Express/public/frontpage/frontpage.js
+++ 1/3._World_Wide_Express/public/frontpage/frontpage.js
@@ -1,1 +1,8 @@
 console.log("The answers to life universe and everything.");
+
+fetch("/amountoffavoriteanimals")
+.then(response => response.json())
+.then(result => {
+    const favoriteAnimalsParagraph = document.getElementById("favorite-animals-amount");
+    favoriteAnimalsParagraph.innerText = result.data;
+});
