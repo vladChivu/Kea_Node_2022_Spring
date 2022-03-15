8b2f44280e78e4d34627fd50f16127418839b582
Anders Latif <andl@kea.dk>
2022-03-15 12:29:15 +0100

    Showed one way to do client routing in Svelte

--- 1/7._Kea_Shop/src/App.svelte
+++ 1/7._Kea_Shop/src/App.svelte
@@ -1,30 +1,23 @@
 <script>
-	export let name;
+	import { Router, Link, Route } from "svelte-navigator";
+
+	import About from "./pages/About/About.svelte"
+	import Home from "./pages/Home/Home.svelte"
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Router>
+	<nav>
+		<Link to="/">Home</Link>
+		<Link to="/about">About</Link>
+	</nav>
+
+	<Route path="/about" component={About}></Route>
+	<Route path="/" component={Home}></Route>
+
+</Router>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	nav {
+		background-color: grey;
 	}
 </style>
