f4eaf5d843f6fb9c4bed894ddee6c922a819063a
Anders Latif <andl@kea.dk>
2022-04-05 13:24:38 +0200

    Created a connection in MySQL

---
+++ 2/9._Football/server/database/createMySQLConnection.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import mysql from "mysql";
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+connection.connect();
+
+export default connection;
---
+++ 2/9._Football/server/database/insertIntoMySQL.js
@@ -0,0 +1,12 @@
+import db from "./createMySQLConnection.js";
+
+/* db.query(`INSERT INTO players (name) VALUES ('Malte')`, (err, data) => {
+    console.log(data);
+
+    db.end();
+
+});
+ */
+
+console.log(results);
+db.end();
