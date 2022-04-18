87cb1faf38261e157e0fa25d08be7371513dbfe2
Anders Latif <andl@kea.dk>
2022-04-18 19:26:34 +0200

    cd ..

--- testingtest/__tests__/mocktest.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require("../app.js");
-
-request(app)
-.get("/api/customparams")
-.send()
-.set('Accept', 'application/json')
-.expect(200, { data: "content " + "customparams" })
-.end((error, request) => {});
--- testingtest/__tests__/unittest.test.js
+++
@@ -1,6 +0,0 @@
-const sum = new Function('a', 'b', 'return a + b');
-
-test('adds 1 and 2 together', () => {
-    expect(sum(1, 2)).toBe(3);
-});
-
