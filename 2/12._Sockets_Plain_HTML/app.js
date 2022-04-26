70e835897a653b0abce8695f953f3d39c7764579
Anders Latif <andl@kea.dk>
2022-04-26 13:43:04 +0200

    Created the scaffold for Svelte and the server

--- 2/12._Sockets_Plain_HTML/app.js
+++ 2/12._Sockets_Plain_HTML/app.js
@@ -10,12 +10,21 @@ import { Server } from "socket.io";
 const io = new Server(server);
 
 io.on("connection", (socket) => {
-    // console.log("A client connected", so cket.id);
+    // console.log("A client connected", socket.id);
 
     socket.on("a client changed the color", ({ data }) => {
-        socket.emit("please change the color", { data });
+
+        // changes the color for OTHER sockets BUT NOT itself
+        // socket.broadcast.emit("please change the color", { data });
+
+        // changes the color ONLY for this socket
+        // socket.emit("please change the color", { data });
+
+        // changes the color for ALL sockets in the io namespace
+        io.emit("please change the color", { data })
     });
 
+    socket.on("disconnect", () => console.log("Bye", socket.id));
 });
 
 
